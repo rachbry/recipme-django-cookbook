915d534fe6608019c7e18b76076a0faa1e1c3bd4
Rachel Cutler <rach_cutler@live.co.uk>
2023-11-30 13:15:14 +0000

    static files and header and footer added

---
+++ home/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import Index
+
+urlpatterns = [
+    path('', Index.as_view(), name='home')
+]
--- my_project/settings.py
+++ my_project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
