f26a6f330c2da5ff71a20acd413a385e309896f0
Rachel Cutler <rach_cutler@live.co.uk>
2023-12-05 21:02:22 +0000

    styling applied to personalised home page

--- static/js/base.js
+++ static/js/base.js
@@ -1,15 +1,34 @@
+// function scrollRecipes(direction) {
+//     console.log('scrollRecipes function called');
+//     const container = document.querySelector('.overflow-auto');
+//     const scrollAmount = 400; // Adjust the scroll amount as needed
 
-function scrollRecipes(direction) {
-    const container = document.querySelector('.overflow-auto');
-    const scrollAmount = 200; // Adjust the scroll amount as needed
+//     if (direction === 'left') {
+//         container.scrollLeft -= scrollAmount;
+//     } else if (direction === 'right') {
+//         container.scrollLeft += scrollAmount;
+//     }
+// }
 
-    if (direction === 'left') {
-        container.scrollLeft -= scrollAmount;
-    } else if (direction === 'right') {
-        container.scrollLeft += scrollAmount;
-    }
+
+function filterCards(mealType) {
+    // Hide all cards
+    const cards = document.querySelectorAll('.recipe-cards .card');
+    cards.forEach(card => {
+        card.style.display = 'none';
+    });
+
+    // Show cards with the selected meal type
+    const filteredCards = document.querySelectorAll(`.recipe-cards .${mealType}`);
+    filteredCards.forEach(card => {
+        card.style.display = 'block';
+    });
 }
 
-function goBack() {
-    window.history.back();
+function showAllCards() {
+    // Show all cards
+    const cards = document.querySelectorAll('.recipe-cards .card');
+    cards.forEach(card => {
+        card.style.display = 'block';
+    });
 }
